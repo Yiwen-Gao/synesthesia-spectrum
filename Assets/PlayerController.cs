1684d45b290faba37a82ba5a31745c1b673d7eea
yiwengao <yiwen.gao@columbia.edu>
2020-02-14 15:55:31 -0500

    added example scene

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 50;
+	private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+
+    void FixedUpdate() {
+    	float horizontalInput = Input.GetAxis("Horizontal");
+    	float verticalInput = Input.GetAxis("Vertical");
+    	Vector3 movement = new Vector3(horizontalInput, 0.0f, verticalInput);
+    	rb.AddForce(movement * speed);
+    }
+}
