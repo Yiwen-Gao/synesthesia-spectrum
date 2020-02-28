8ad4028494206e668dfed266a9c45e9e73d3182c
mwinitch <michaelwin26@gmail.com>
2020-02-28 15:05:02 -0500

    Basic gravity changing where pressing keys 'g', 't', and 'o' will change the gravity

--- Assets/MapMover.cs
+++ Assets/MapMover.cs
@@ -6,19 +6,59 @@ public class MapMover : MonoBehaviour
 {
     // Start is called before the first frame update
     public float angle = 180;
+    int currentPosition;
     public GameObject Map;
 
     void Start()
     {
         Map = GameObject.Find("Map");
+        currentPosition = 0;
     }
 
+    //T corresponds to turquoise wall (currentPosition = 1)
+    //O corresponds to orange wall (currentPosition = 2)
+    //G correspond to gray which is starting ground (currentPosition = 0)
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if ((Input.GetKeyDown(KeyCode.G)) && (currentPosition == 1))
         {
-            Map.transform.Rotate(0, 0, angle);
+            Map.transform.Rotate(0, 0, 270);
+            currentPosition = 0;
+
+        }
+
+        if ((Input.GetKeyDown(KeyCode.O)) && (currentPosition == 1))
+        {
+            Map.transform.Rotate(0, 0, 180);
+            currentPosition = 2;
+
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.O) && (currentPosition == 0))
+        {
+            Map.transform.Rotate(0, 0, 270);
+            currentPosition = 2;
+            
+        }
+
+        if (Input.GetKeyDown(KeyCode.T) && (currentPosition == 0))
+        {
+            Map.transform.Rotate(0, 0, 90);
+            currentPosition = 1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.T) && (currentPosition == 2))
+        {
+            Map.transform.Rotate(0, 0, 180);
+            currentPosition = 1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.G) && (currentPosition == 2))
+        {
+            Map.transform.Rotate(0, 0, 90);
+            currentPosition = 0;
         }
     }
 }
