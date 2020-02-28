1d5bd9ff2ac1244251fd9ad76517bfece577e22d
yiwengao <yiwen.gao@columbia.edu>
2020-02-28 16:18:46 -0500

    Added tutorial level

--- Assets/MapMover.cs
+++ Assets/MapMover.cs
@@ -2,23 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MapMover : MonoBehaviour
-{
+public class MapMover : MonoBehaviour {
     // Start is called before the first frame update
     public float angle = 180;
     public GameObject Map;
 
-    void Start()
-    {
+    void Start() {
         Map = GameObject.Find("Map");
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Map.transform.Rotate(0, 0, angle);
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            // Map.transform.RotateAround(0, 0, angle);
         }
     }
 }
---
+++ Assets/Scripts/ObstacleController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleController : MonoBehaviour {
+
+	public float speed;
+	private float dir;
+	public float offsetZ;
+
+    // Start is called before the first frame update
+    void Start() {
+        dir = 1;
+    }
+
+    // Update is called once per frame
+    void Update() {
+    	Vector3 offset = new Vector3(0f, 0f, dir * offsetZ);
+        transform.position += offset;
+    }
+
+    void OnCollisionEnter(Collision other) {
+    	if (other.gameObject.CompareTag("Wall")){
+    		dir = -dir;
+    	}
+    }
+}
