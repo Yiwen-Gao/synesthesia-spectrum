6b41c7bdbc5b145ccb212a14f59c4a3b5a8937f1
yiwengao <yiwen.gao@columbia.edu>
2020-04-03 14:52:05 -0500

    Added tunnel movement to level1 scene

--- Assets/Scripts/ObstacleController.cs
+++ Assets/Scripts/ObstacleController.cs
@@ -6,21 +6,26 @@ public class ObstacleController : MonoBehaviour {
 
 	public float speed;
     public Vector3 dir;
-	private float offset;
+    private Random rand;
 
     // Start is called before the first frame update
     void Start() {
-        offset = .1f;
+        rand = new Random();
     }
 
     // Update is called once per frame
     void Update() {
-        transform.position += offset * dir;
+        transform.position += speed * dir;
     }
 
     void OnCollisionEnter(Collision other) {
-    	if (other.gameObject.CompareTag("Wall")){
-    		dir = -dir;
-    	}
+    	// if ((dir.x != 0 && other.gameObject.CompareTag("Vertical Wall")) || 
+     //        (dir.y != 0 && other.gameObject.CompareTag("Horizontal Wall"))) {
+    	// 	dir = -dir;
+    	// }
+
+        if (other.gameObject.CompareTag("Vertical Wall") || other.gameObject.CompareTag("Horizontal Wall")) {
+            dir = -dir;
+        }
     }
 }
---
+++ Assets/Scripts/TunnelController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TunnelController : MonoBehaviour {
+
+	public GameObject[] walls;
+	private int currIdx;
+
+    // Start is called before the first frame update
+    void Start() {
+        currIdx = 0;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Space))
+        	transform.Rotate(90, 0, 0);
+    }
+
+    private void onTriggerEnter(Collider other) {
+    	
+    	transform.Rotate(0, 0, -90);
+    	// Debug.Log("collision");
+    	// if (other.gameObject.CompareTag("Player")) {
+    	// 	GameObject wall = other.contacts[0].thisCollider.gameObject;
+    	// 	int prevIdx = (currIdx - 1) % walls.Length;
+	    // 	int nextIdx = (currIdx + 1) % walls.Length;
+	    	
+	    // 	if (wall == walls[prevIdx]) {
+	    // 		transform.Rotate(0, 0, -90);
+	    // 		currIdx = prevIdx;
+	    // 	}
+	    // 	else if (wall == walls[nextIdx]) {
+	    // 		transform.Rotate(0, 0, 90);
+	    // 		currIdx = nextIdx;
+	    // 	}
+	    // }
+    }
+}
