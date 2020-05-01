69c11689e4c215ceeb2c7200c696c17d6039a622
yiwengao <yiwen.gao@columbia.edu>
2020-05-01 14:45:21 -0500

    added menu scene

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+	public string singlePlayerScene;
+	public string multiPlayerScene;
+
+    public void StartSinglePlayer() 
+    {
+    	SceneManager.LoadScene(singlePlayerScene);
+    }
+
+    public void StartMultiPlayer()
+    {
+    	SceneManager.LoadScene(multiPlayerScene);
+    }
+}
