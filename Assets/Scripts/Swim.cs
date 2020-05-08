c7e3cc7699b4c15ef43a6bb35b18d350347e7a37
ile123ile <bbravencofounder@gmail.com>
2020-05-08 12:22:15 -0700

    added swim in circle script

---
+++ Assets/Scripts/Swim.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swim : MonoBehaviour
+{
+    public float loopTime = 5;
+    public Vector3 center;
+    private float radius;
+    private float currTime;
+
+    private void Start()
+    {
+        center.y = transform.position.y;
+        radius = (transform.position - center).magnitude;
+        currTime = loopTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currTime += Time.deltaTime;
+        if (currTime >= loopTime)
+        {
+            currTime -= loopTime;
+        }
+        transform.position = center + radius * new Vector3(Mathf.Cos(2 * Mathf.PI * currTime / loopTime), 0, Mathf.Sin(2*Mathf.PI * currTime / loopTime));
+        transform.LookAt(center);
+    }
+}
